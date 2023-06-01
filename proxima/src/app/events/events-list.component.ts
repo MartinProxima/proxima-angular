3a27b293783bf0c099b4bdc11a3aee2a4f1242a0
Martin <martin@viar.si>
2023-06-01 18:29:32 +0200

    Added output decorator

--- proxima/src/app/events/events-list.component.ts
+++ proxima/src/app/events/events-list.component.ts
@@ -6,23 +6,13 @@ import { Component } from "@angular/core";
   <div>
     <h1>Upcoming events</h1>
     <hr>
-    <div class="well hoverwell thumbnail">
-      <h2>{{event.name}}</h2>
-      <div>Date: {{event.date}}</div>
-      <div>Time: {{event.time}}</div>
-      <div>Price: \${{event.price}}</div>
-      <div>
-        <span>Location: {{event.location.address}}</span>
-        <span>&nbsp;</span>
-        <span>{{event.location.city}}, {{event.location.country}}</span>
-      </div>
-    </div>
+    <event-thumbnail [event]="event1" (eventClick)="handleEventClicked($event)"></event-thumbnail>
   </div>
   `
 })
 
 export class EventsListComponent {
-  event = {
+  event1 = {
     id: 1,
     name: 'Angular Connect',
     date: '26/09/2036',
@@ -34,5 +24,9 @@ export class EventsListComponent {
       city: 'London',
       country: 'England'
     }
+  }
+
+  handleEventClicked(data: string){
+    console.log(data)
   }
 }
