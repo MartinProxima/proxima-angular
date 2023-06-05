73f5b30765853b554282093997e64f0832764073
Martin Coretti <martin.coretti@gmail.com>
2023-06-05 13:49:28 +0200

    Added routing and navigation

--- proxima/src/app/app.module.ts
+++ proxima/src/app/app.module.ts
@@ -1,21 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver
+} from './events/index'
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavbarComponent } from './nav/nav-bar.component';
+import { ToastrService } from './common/toastr.service';
+import { appRoutes } from 'src/routes';
+import { RouterModule } from '@angular/router';
+import { Error404Component } from './errors/404.components';
 
 @NgModule({
+  providers: [
+    EventService,
+    ToastrService,
+    EventRouteActivator,
+    { provide: 'canDeactivateCreateEvent', useValue: checkDirtyState },
+    EventListResolver,
+  ],
   declarations: [
     EventsAppComponent,
     EventsListComponent,
     EventThumbnailComponent,
-    NavbarComponent
+    EventDetailsComponent,
+    NavbarComponent,
+    CreateEventComponent,
+    Error404Component,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
+
+export function checkDirtyState(component: CreateEventComponent) {
+  if(component.isDirty) return window.confirm('You have not saved this event, do you really want to cancel?')
+  else return true
+}
